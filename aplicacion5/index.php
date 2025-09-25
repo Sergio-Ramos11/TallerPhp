7db3fdef3c6d24d5f3fa6f2c55bfd71caff5b279
Sergio Ramos <serramos@uniboyaca.edu.co>
2025-09-25 15:25:12 -0500

    Arreglado un pequeño error de sintáxis

--- aplicacion5/index.php
+++ aplicacion5/index.php
@@ -8,12 +8,12 @@
 </head>
 
 <body>
-    <h1>Convertidor de numero decimal a binario</h1>
+    <h1>Convertidor de numero entero a binario</h1>
 
     <div>
         <form method="POST">
-            <label for="decimal">Escriba el número decimal a convertir:</label>
-            <input type="text" id="decimal" name="decimal" placeholder="Numero decimal" required>
+            <label for="entero">Escriba el número entero a convertir:</label>
+            <input type="text" id="entero" name="entero" placeholder="Numero entero" required>
 
             <button type="submit">Enviar</button>
         </form>
@@ -21,19 +21,19 @@
 
     <?php
     if ($_SERVER['REQUEST_METHOD'] === "POST") {
-        $decimal = $_POST['decimal'];
-        $binario = decbin($decimal);
-        //$binario = convertir($decimal);
-        echo "<h2>El numero decimal $decimal en binario es: $binario</h2>";
+        $entero = $_POST['entero'];
+        $binario = decbin($entero);
+        //$binario = convertir($entero);
+        echo "<h2>El numero entero $entero en binario es: $binario</h2>";
     }
 
 
-    /*function convertir($decimal)
+    /*function convertir($entero)
     {
         $binario = "";
-        while ($decimal > 0) {
-            $binario = ($decimal % 2) . $binario; // resto concatenado a la izquierda
-            $decimal = intdiv($decimal, 2);       // división entera por 2
+        while ($entero > 0) {
+            $binario = ($entero % 2) . $binario; // resto concatenado a la izquierda
+            $entero = intdiv($entero, 2);       // división entera por 2
         }
         return $binario; // por si el número era 0
     }*/
